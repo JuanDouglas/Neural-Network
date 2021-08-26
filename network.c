8f062c2ef1743e03005bea80cda3843119de1fed
Juan Douglas <juandouglas2004@gmail.com>
2021-08-26 11:23:51 -0300

    Add network file

---
+++ network.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void CalculeLayer(float *layer[])
+{
+    int len = sizeof(&layer) / sizeof(&layer[0]);
+
+    for (int i = 0; i < len; i++)
+    {
+      
+    }
+}
+
+int main(){
+    printf("Ok!");
+}
